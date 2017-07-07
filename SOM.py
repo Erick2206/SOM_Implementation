35f6c581a8305c94c44738490f8abec5eb753042
Aayush Gogia <aayushar@yahoo.com>
2017-07-07 11:22:12 +0530

    Added template for SOM Implementation

--- SOM.py
+++ SOM.py
@@ -3,6 +3,7 @@ This file is an implementation of the SOM algorithm
 '''
 
 import numpy as np
+from pprint import pprint
 
 #Kernel Parameters
 size_x=8			#size of 1 kernel
@@ -12,15 +13,31 @@ size_y=8
 nKernel_x=8			#No of kernels on x and y axis on a SOM Map.
 nKernel_y=8
 
-class kernel:
-	def __init__(self, size_y, size_x):
-		#Randomly initialized kernel
-		self.kernel=np.random.rand(size_x,size_y)
-		
+
 class SOM_Map:
-	def __init__(self,nKernel_y,nKernel_x):
+	def __init__(self,nKernel_y,nKernel_x,size_y,size_x):
+		self.map=[[self.get_kernel(size_y,size_x) for j in range(nKernel_y)] for i in range(nKernel_x)]
+		s
 
+	def get_kernel(size_y,size_x):
+		'''
+		Returns a kernel of size_y rows and size_x columns
+		'''
+		kernel=np.random.rand(size_x,size_y)
+		return kernel
 
+	def self.getDistance():
+		
+
+	def self.fit():
+		D=self.getDistance(a,b)
+
+	def self.updateWeights():
+		#TODO
+
+	def self.getNeighbours():
+		#TODO
 
 if __name__=="__main__":
-	k=kernel(size_y,size_x)
+	som=SOM_Map(nKernel_x,nKernel_y,size_y,size_x)
+	pprint(som.map)
