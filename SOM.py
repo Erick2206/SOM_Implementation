74c94d70c26840ac6ee04292c39dcadfa99c1ced
Aayush Gogia <aayushar@yahoo.com>
2017-07-09 01:59:21 +0530

    Continuing work on SOM

--- SOM.py
+++ SOM.py
@@ -16,6 +16,7 @@ nKernel_y=8
 #Training parameters
 learning_rate=0.1
 sigma=0.3
+
 inp=np.random.rand(size_x,size_y)
 
 
@@ -27,25 +28,30 @@ class SOM_Map:
 		self.size_x=size_x
 		self.learning_rate=learning_rate
 		self.sigma=sigma
+
+		#Total no. of iterations for the model
 		self.num_iteration=num_iteration
+
+		#Iteration count for finding the value of sigma
+		self.iterationCount=1
 
 		self.map=self.setRandomWeights()
 
-	def self.setRandomWeights():
+	def setRandomWeights(self):
 		'''
 		Returns a map of Randomly initiated weights
 		'''
 		kernels=[[self.get_kernel(size_y,size_x) for j in range(nKernel_y)] for i in range(nKernel_x)]
 		return kernels
 
-	def self.get_kernel(size_y,size_x):
+	def get_kernel(self,size_y,size_x):
 		'''
 		Returns a kernel of size_y rows and size_x columns
 		'''
-		kernel=np.random.rand(size_x,size_y)
+		kernel=np.random.random_sample((self.size_y,self.size_x))
 		return kernel
 
-	def self.getDistance(a,b):
+	def getEuclideanDistance(self,a,b):
 		'''
 		Returns the Euclidean Distance between 2 Matrices
 		'''
@@ -56,14 +62,72 @@ class SOM_Map:
 
 		return np.sqrt(total)
 
-	def self.fit():
-		D=self.getDistance(a,b)
+	def findBestMatchingUnit(self):
+		'''
+		This function finds the winning neuron
+		'''
+ 		euclidDist=99999
+ 		BMUCoordinates=[]
 
-	def self.updateWeights():
-		#TODO
+ 		for i in range(nKernel_y):
+ 			for j in range(nKernel_x):
+ 				kernelDist=self.getEuclideanDistance(self.map[i][j],inp) #Need work here because there is no input dataset at present
 
-	def self.getNeighbours():
-		#TODO
+ 				if kernelDist<euclidDist:
+ 					euclidDist=kernelDist
+ 					BMUCoordinates=[i,j]
+
+ 		return BMUCoordinates
+
+	def fit(self):
+		'''
+		Primary starting function of the Self Organizing Maps Network
+		Similar to fit function of sklearn library
+		'''
+		for i in range(self.num_iteration):
+			BMU=self.findBestMatchingUnit()
+			self.updateWeights(BMU)
+
+	def updateWeights(self):
+		'''
+		Updates the weight of the neurons of the SOM Map
+		'''
+
+		for i in range(nKernel_y):
+			for j in range(nKernel_x):
+				dist=self.distanceBetweenKernels(BMU,[i,j])
+				eta=np.exp(-(dist)**2/(2*(self.sigma)**2))
+				self.learningRateDecay()
+				self.map[i][j]+=(eta*self.learning_rate*(inp-self.map[i][j]))
+		return eta
+
+	def getNeighbourRadius(self):
+		'''
+		Returns the radius of the neighbours to update the weights
+		'''
+		self.sigma=max(nKernel_x,nKernel_y)/2
+		self.timeConstant=num_iteration/np.log(self.sigma)
+		self.sigma=self.sigma * np.exp(-self.iterationCount/float(self.timeConstant))
+		self.iterationCount+=1
+		return self.sigma
+
+	def learningRateDecay(self):
+		'''
+		Decay function for Learning Rate
+		'''
+		self.learning_rate=self.learning_rate * np.exp(-self.iterationCount/float(self.timeConstant))
+
+	def distBetweenKernels(self,a,b):
+		'''
+		Returns the distance between 2 kernels
+		:params
+		a: Co-ordinates of winning neuron
+		b: Co-ordinates of neuron with whom its distance is calculated
+		'''
+		dist=np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
+		return dist
+
+
 
 if __name__=="__main__":
 	som=SOM_Map(nKernel_x,nKernel_y,size_y,size_x,learning_rate,sigma)
