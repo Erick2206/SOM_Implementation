62538e1e25b0464b396b8b32c98b8effce0600ca
Aayush Gogia <aayushar@yahoo.com>
2017-07-31 16:25:55 +0530

    Completed work on accuracy.

--- accuracy.py
+++ accuracy.py
@@ -1,18 +1,12 @@
 from minisom import MiniSom
 from pprint import pprint
 import numpy as np
+from SOM import CorrCoef_Max_pooling_Layer2_3
+from os.path import exists
 
 map_x=10
 map_y=10
-
-pprint (map_dict)
-print len(map_dict[0])
-'''
-for cnt,xx in enumerate(x_train):
-    w=som.winner(xx)
-    print w
-    break
-'''
+size_x=3
 
 class TrainCorrCoef:
     def __init__(self,corrCoef,map_y,map_x):
@@ -22,25 +16,56 @@ class TrainCorrCoef:
 
     def train(self):
         som=MiniSom(self.map_y,self.map_x,54,sigma=1.0,learning_rate=0.5)
-        som.random_weights_init(data)
+        som.random_weights_init(corrCoef)
 
         print("Training")
-        som.train_random(data,100)
+        som.train_random(corrCoef,100)
 
         print("\nReady!!!")
 
         return som
 
+
 class Accuracy:
-    def __init__(self,som,x_test,y_test):
+    def __init__(self,corrCoef,som,y_train,x_test,y_test):
+        self.corrCoef=corrCoef
         self.som=som
+        self.y_train=y_train
         self.x_test=x_test
         self.y_test=y_test
         self.map_dict=[[{0:0,1:0,'s':-1} for j in range(map_x) ]for i in range(map_y)]
 
     def findAccuracy(self):
+        for cnt,xx in enumerate(self.corrCoef):
+            w=som.winner(xx)
+            self.map_dict[w[0]][w[1]][self.y_train[cnt]]+=1
 
-        print "Accuracy of the model is",
+        for q in range(map_y):
+            for p in range(map_x):
+                if self.map_dict[q][p][0]>self.map_dict[q][p][1]:
+                    self.map_dict[q][p]['s']=0
+                else:
+                    self.map_dict[q][p]['s']=1
+
+        if not exists('corrCoefListTest.npy'):
+            trained_weights=np.load('trained_weights.npy')
+            cc=CorrCoef_Max_pooling_Layer2_3(self.x_test,trained_weights,size_x)
+            corrCoefListTest=cc.run()
+            np.save('corrCoefListTest',corrCoefListTest)
+
+        else:
+            np.load('corrCoefListTest.npy')
+
+        correct=0
+
+        for cnt,xx in enumerate(corrCoefListTest):
+            w=som.winner(xx)
+            if self.map_dict[w[0]][w[1]]['s']==self.y_test[cnt]:
+                correct+=1
+
+        accuracy=float(correct)/len(y_test)
+
+        print "Accuracy of the model is %.3f" % (accuracy)
 
 if __name__=='__main__':
     '''
@@ -55,5 +80,5 @@ if __name__=='__main__':
     '''
     #Load data
     x_train, y_train, x_test, y_test, vocabulary_inv= np.load('input.npy')
-    acc=Accuracy(som,x_test,y_test)
+    acc=Accuracy(corrCoef,som,y_train,x_test,y_test)
     acc.findAccuracy()
